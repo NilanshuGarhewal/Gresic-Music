a037f3d53d7e5e7a295a9513f984e3cef27dba4b
Nilanshu Garhewal <nilanshugarhewal1@gmail.com>
2025-09-04 12:20:28 +0530

    Strcutured the files & folders.

--- server/dist/data/Init.js
+++ server/dist/data/Init.js
@@ -1,44 +1,31 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-// IMPORTING STUFF
-const mongoose_1 = __importDefault(require("mongoose"));
-const Data_1 = __importDefault(require("./Data"));
-const Beat_1 = __importDefault(require("../model/Beat"));
-const node_console_1 = require("node:console");
-const dotenv_1 = __importDefault(require("dotenv"));
-// --------------------------------------------->
-// --------------------------------------------->
-// --------------------------------------------->
-// USING STUFF
-dotenv_1.default.config();
-// --------------------------------------------->
-// --------------------------------------------->
-// --------------------------------------------->
-// CONNETING DB
-const URL = process.env.MONGO_URI;
-mongoose_1.default
-    .connect(URL)
-    .then(() => console.log("MongoDB Connected"))
-    .catch((err) => console.error(err));
-// --------------------------------------------->
-// --------------------------------------------->
-// --------------------------------------------->
-// INITIALIZING DB
-const initializeData = () => __awaiter(void 0, void 0, void 0, function* () {
-    yield Beat_1.default.deleteMany({});
-    yield Beat_1.default.insertMany(Data_1.default);
-    (0, node_console_1.log)("Data is inserted in Database.");
-});
-initializeData();
+// // IMPORTING STUFF
+// import mongoose from "mongoose";
+// // import { testingData } from "./Data";
+// import Beat from "../model/Beat";
+// import { log } from "node:console";
+// import dotenv from "dotenv";
+// // --------------------------------------------->
+// // --------------------------------------------->
+// // --------------------------------------------->
+// // USING STUFF
+// dotenv.config();
+// // --------------------------------------------->
+// // --------------------------------------------->
+// // --------------------------------------------->
+// // CONNETING DB
+// const URL = process.env.MONGO_URI!;
+// mongoose
+//   .connect(URL)
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch((err) => console.error(err));
+// // --------------------------------------------->
+// // --------------------------------------------->
+// // --------------------------------------------->
+// // INITIALIZING DB
+// const initializeData = async () => {
+//   await Beat.deleteMany({});
+//   await Beat.insertMany(testingData);
+//   log("Data is inserted in Database.");
+// };
+// initializeData();
